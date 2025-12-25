251384cd5eb510fede80a3f98e05b48a378f9db3
elyse502 <elyseniyibizi502@gmail.com>
2025-12-25 12:06:53 +0200

    feat: trigger email notification on task assignment
        - Integrate Inngest event emission in createTask controller
    
        - Send 'app/task.assigned' event when task is created and assigned
    
        - Include task ID and origin data for email notification context
    
        - Trigger automated email workflow for task assignment notifications

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,5 @@
 import prisma from "../configs/prisma.js";
+import { inngest } from "../inngest/index.js";
 
 // Create task
 export const createTask = async (req, res) => {
@@ -52,6 +53,14 @@ export const createTask = async (req, res) => {
     const taskWithAssignee = await prisma.task.findUnique({
       where: { id: task.id },
       include: { assignee: true },
+    });
+
+    await inngest.send({
+      name: "app/task.assigned",
+      data: {
+        taskId: task.id,
+        origin,
+      },
     });
 
     res.json({ task: taskWithAssignee, message: "Task created successfully" });
